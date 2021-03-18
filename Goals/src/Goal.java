0f71e63673a64b67763cf3bb1b5748f56e1c33e1
I-SA33 <80543990+I-SA33@users.noreply.github.com>
2021-03-18 14:06:54 +0000

    Add files via upload

--- Goals/src/Goal.java
+++ Goals/src/Goal.java
@@ -5,65 +5,22 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Goal {
-    private float weight;
-    private LocalDate date1;
+    private final double weight;
+    private final LocalDate date1;
     private final LocalDate date2 = LocalDate.now();
-    private boolean isComplete;
-    private double distance;
-    private LocalTime time;
+    public boolean isComplete;
 
-    public Goal(){
-        setWeight();
-        setDate();
+    //constructor
+    public Goal(double weight, String date){
+        this.weight = weight;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy");
+        this.date1 = LocalDate.parse(date, formatter);
         runningGoal();
-        goalCheck();
     }
 
-    public void setWeight() {
-        System.out.print("Enter a weight goal in kg: ");
-        Scanner scanner = new Scanner(System.in);
-        this.weight = scanner.nextFloat();
-    }
-
-    public void setDate() {
-        System.out.println("Enter date to complete this goal (dd-mm-yy): ");
-        Scanner scan=new Scanner(System.in);
-        String date=scan.next();
-        //checks if the date is valid and stores or outputs error
-        try{
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy");
-            this.date1 = LocalDate.parse(date, formatter);
-        }catch (DateTimeException e){
-            System.err.println("Invalid date, Make sure you enter the correct date format\n");
-        }
-    }
-
-    public float getWeight(){
-        return weight;
-    }
-
-    public LocalDate getDate() {
-        return date1;
-    }
-
-    public double getDistance(){
-        return distance;
-    }
-
-    public LocalTime getTime(){
-        return time;
-    }
-
-    public boolean getStatus() {
-        return isComplete;
-    }
-
-    public void goalCheck() {
-        System.out.println("Enter you current weight: ");
-        Scanner sc = new Scanner(System.in);
-        float currentWeight = sc.nextFloat();
+    public void goalCheck(double currentWeight) {
         if (date1.isAfter(date2)) {
-            if(currentWeight == getWeight()){
+            if(currentWeight == weight){
                 System.out.println("Congratulations you've hit your weight goal!");
                 System.out.println("But you have exceeded you goal date\n");
                 isComplete = true;
@@ -80,7 +37,7 @@ public class Goal {
             }
         }
         if (date1.isBefore(date2)) {
-            if(currentWeight == getWeight()){
+            if(currentWeight == weight){
                 System.out.println("Congratulations you've hit your weight goal!");
                 System.out.println("You've beaten your date, why not set another goal\n");
                 isComplete = true;
@@ -89,7 +46,7 @@ public class Goal {
             }
         }
         if (date1.isEqual(date2)) {
-            if(currentWeight == getWeight()){
+            if(currentWeight == weight){
                 System.out.println("Congratulations you've hit your weight goal!");
                 System.out.println("Just in time\n");
                 isComplete = true;
@@ -122,7 +79,6 @@ public class Goal {
             System.out.println("Enter distance ");
             new Scanner(System.in);
             double dist = scanner.nextDouble();
-            this.distance = dist;
             System.out.println("Enter time (HH:mm:ss) ");
             Scanner sc = new Scanner(System.in);
             //store dist and time
@@ -131,37 +87,11 @@ public class Goal {
             try{
                 DateTimeFormatter df = DateTimeFormatter.ofPattern("HH:mm:ss");
                 LocalTime localTime = LocalTime.parse(time, df);
-                this.time = localTime;
                 System.out.println("Distance set for: " + dist + " Time set for: " + localTime + "\n");
             }catch (DateTimeException e){
                 System.err.println("Invalid date, Make sure you enter the correct date format\n");
             }
         }
-    }
-
-    public void setNewGoal(){
-        String ans;
-        boolean repeat = true;
-        while (repeat) {
-            System.out.println("Do you want to set new goal?");
-            Scanner scanner = new Scanner(System.in);
-            ans = scanner.nextLine();
-            if (ans.equalsIgnoreCase("Yes")) {
-                new Goal();
-                repeat = false;
-            }
-            else if (ans.equalsIgnoreCase("No")) {
-                System.out.println("Go smash those goals");
-                repeat = false;
-            }else{
-                System.out.println("\nInvalid try again ... \n");
-            }
-        }
-    }
-
-    public static void main(String[] args){
-        Goal goal = new Goal();
-        goal.setNewGoal();
     }
 }
 
