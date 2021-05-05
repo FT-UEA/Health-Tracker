defe9a2ab0bb9d9c64c4bf12bbe00e66b5824aa6
unknown <wwc19tzu@uea.ac.uk>
2021-05-05 13:26:44 +0100

    Modified to add one extra variavle from to help with emails

--- Group/src/Goal.java
+++ Group/src/Goal.java
@@ -1,6 +1,13 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Goal implements Serializable {
+    int hello = 10;
+    int dog = 9;
+    ArrayList<String> array = new ArrayList<>();
+    String bob = "BOB";
+    String from;
+
     Goal(){
 
     }
